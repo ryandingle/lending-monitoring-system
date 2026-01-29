bbc872a944855152e80db7681aa32199b8037199
Ryan <ryandingle09@gmail.com>
2026-01-30 01:22:37 +0800

    adjustment on amplify build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  experimental: {
-    turbo: false,
-  },
+  poweredByHeader: false
 };
 
 export default nextConfig;
